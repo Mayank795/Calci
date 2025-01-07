08881899972dc9b48335aac2c89f8607a97a6b39
Mayank795 <mayankspal555@gmail.com>
2025-01-07 14:41:57 +0530

    Addition Functionality

--- Calci.java
+++ Calci.java
@@ -10,6 +10,9 @@ public class Calci {
 
         System.out.print("Enter the second number: ");
         double num2 = scanner.nextDouble();
+	
+	double sum = num1 + num2; 
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
 
         scanner.close();
     }
